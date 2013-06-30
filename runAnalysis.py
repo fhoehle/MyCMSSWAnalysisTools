a9ac876b85512b33729a65e2380701b86624db2d
Felix Hoehle <hoehle@physik.rwth-aachen.de>
2013-06-30 18:44:23 +0200

    added class sample

--- runAnalysis.py
+++ runAnalysis.py
@@ -1,3 +1,15 @@
 # run cmssw analysis
+import sys
+class sample():
+  def __init__(self,name):
+    self.name = name
+  def xSec(self,xSec):
+    self.xSec = float(xSec)
+  def numEvts(self,numEvts):
+    self.numEvts=float(numEvts)
+  def intLumi(self):
+    if self.numEvts and self.xSec:
+      return  self.numEvts/self.xSec
 def runOverSample (cfg,samp):
   pass
+
