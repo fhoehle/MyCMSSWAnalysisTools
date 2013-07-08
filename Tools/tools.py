d49f1873129bb459c99f62271e79e8d49b3a9fef
Felix Hoehle <hoehle@physik.rwth-aachen.de>
2013-07-08 12:00:46 +0200

    added outputlocation

--- Tools/tools.py
+++ Tools/tools.py
@@ -1,6 +1,9 @@
 # tools for run cmssw analysis
 import FWCore.ParameterSet.Config as cms
 import sys,imp,subprocess,re,os
+def addPostFixToFilename(name,postfix):
+  reName = re.match('(.*[^\.])(\.[^ \.]*)',name)
+  return reName.group(1) + (("_"+postfix) if postfix != "" else "") +reName.group(2)
 #########################
 class sample():
   def __init__(self,name,postfix = "",maxEvents=-1):
@@ -30,55 +33,55 @@ class processSample():
     self.tmpCfg = None
   def createTmpCfg(self):
     if self.tmpCfg != None:
-      print "its already there"
       return 
     cfgFile = open(self.cfgFileName,'r') #copy.deepcopy(f);f.close()
     cfgFileLoaded = imp.load_source('cfgTMP',self.cfgFileName,cfgFile);cfgFile.close()
-    reTmpCfgFileName =  re.match('(.*[^\.])(\.[^ \.]*)',self.cfgFileName)
-    tmpCfg = open(reTmpCfgFileName.group(1)+'_TMP'+reTmpCfgFileName.group(2) , 'w')
+    tmpCfg = open(addPostFixToFilename(self.cfgFileName,'TMP') , 'w')
     tmpCfg.write(cfgFileLoaded.process.dumpPython())
     tmpCfg.close()
     self.tmpCfg = tmpCfg.name
-  def createNewCfg (self,samp,putNewCfgHere):
+  # create new config, i.e. apply sample specific changes: inputFiles output TFileService MessageLogger
+  def createNewCfg (self,samp,putNewCfgHere,additionalOutputFolder=''):
     self.createTmpCfg()
     import copy
-    #f = open(self.cfgFileName,'r')
-    print "laoding ",self.tmpCfg
-    tmpCfgFile = open(self.tmpCfg,'r') #copy.deepcopy(f);f.close()
+    tmpCfgFile = open(self.tmpCfg,'r') 
     tmpCfgFileLoaded = imp.load_source('cfg'+samp.postfix,self.tmpCfg,tmpCfgFile);tmpCfgFile.close()
-#    cfgFileLoaded.process.MessageLogger
     #adapt input
     tmpCfgFileLoaded.process.source.fileNames = samp.getInputfiles();tmpCfgFileLoaded.process.maxEvents.input.setValue(samp.maxEvents)
+    additionalOutputFolder = 'file:'+additionalOutputFolder
     #adapt output
     for outItem in tmpCfgFileLoaded.process.outputModules.values():
-        reOutName = re.match('(.*)(\.[^ \.]*)',outItem.fileName.value())
-        outItem.fileName.setValue(reOutName.group(1)+ (("_"+samp.postfix) if samp.postfix != "" else "") + reOutName.group(2) )
+        outItem.fileName.setValue(additionalOutputFolder+addPostFixToFilename(outItem.fileName.value(),samp.postfix))
     # TFileService
     if hasattr(tmpCfgFileLoaded.process,"TFileService"):
-      reTFileServiceName = reOutName = re.match('(.*)(\.[^ \.]*)', tmpCfgFileLoaded.process.TFileService.fileName.value())
-      tmpCfgFileLoaded.process.TFileService.fileName.setValue (reTFileServiceName.group(1)+ (("_"+samp.postfix) if samp.postfix != "" else "") +reTFileServiceName.group(2))
+      tmpCfgFileLoaded.process.TFileService.fileName.setValue(additionalOutputFolder+addPostFixToFilename(tmpCfgFileLoaded.process.TFileService.fileName.value(),samp.postfix))
     # MessageLogger
     if hasattr(tmpCfgFileLoaded.process,"MessageLogger"):
       for dest in tmpCfgFileLoaded.process.MessageLogger.destinations:
         if hasattr(tmpCfgFileLoaded.process.MessageLogger,dest):
           if hasattr(getattr(tmpCfgFileLoaded.process.MessageLogger,dest),'filename'): 
-            reMessageLoggerFilename = re.match('(.*)(\.[^ \.]*)',getattr(getattr(tmpCfgFileLoaded.process.MessageLogger,dest),'filename').value())
-            getattr(getattr(tmpCfgFileLoaded.process.MessageLogger,dest),'filename').setValue(reMessageLoggerFilename.group(1)+(("_"+samp.postfix) if samp.postfix != "" else "") + reMessageLoggerFilename.group(2))     
+            getattr(getattr(tmpCfgFileLoaded.process.MessageLogger,dest),'filename').setValue(additionalOutputFolder+addPostFixToFilename(getattr(getattr(tmpCfgFileLoaded.process.MessageLogger,dest),'filename').value(),samp.postfix))     
     # create new cfg
-    reCfgFileName =  re.match('(.*[^\.])(\.[^ \.]*)',self.cfgFileName)
-    newCfgFileName=reCfgFileName.group(1) + (("_"+samp.postfix) if samp.postfix != "" else "") +"_createdBy_runOverSample"+reCfgFileName.group(2)
+    newCfgFileName= addPostFixToFilename(self.cfgFileName , samp.postfix) 
     newCfgFileName = re.match('.*[^\/]\/([^\/][^\/]*)',newCfgFileName).group(1) if putNewCfgHere else newCfgFileName
     newCfg = open(newCfgFileName , 'w')
     newCfg.write(tmpCfgFileLoaded.process.dumpPython())
     print newCfg.name
     newCfg.close()
-    del tmpCfgFileLoaded
-    del tmpCfgFile
     self.newCfgName = newCfg.name
-  def runSample(self,samp,putNewCfgHere):
-    self.createNewCfg (samp,putNewCfgHere)
+  # process sample
+  def runSample(self,samp,putNewCfgHere,additionalOutputFolder=''):
+    print additionalOutputFolder
+    if not additionalOutputFolder == '' and not additionalOutputFolder == None:
+      if additionalOutputFolder.endswith('/'):
+         additionalOutputFolder +='/'  
+      if os.path.exists(additionalOutputFolder):
+        print "Warning folder exists"
+      else :
+        os.makedirs(additionalOutputFolder) 
+    self.createNewCfg (samp,putNewCfgHere,additionalOutputFolder)
     command="cmsRun "+ self.newCfgName +' >& '+self.newCfgName+"_output.log"
-    print command
+    print command,"  outputFolder ",additionalOutputFolder
     subPrOutput = subprocess.Popen([command],shell=True,stdout=subprocess.PIPE,env=os.environ)
     subPrOutput.wait()
     errorcode = subPrOutput.returncode
