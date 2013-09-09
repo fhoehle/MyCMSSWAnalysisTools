37c3d2e54ed13c2dcf84fe82567ef0d34adf62e5
Felix Hoehle <hoehle@physik.rwth-aachen.de>
2013-09-09 23:11:54 +0200

    first version of processed events, maxEvents from arguments if -1 dbsquery

--- processedEvents.py
+++ processedEvents.py
@@ -1,65 +1,48 @@
 #!/usr/bin/env python
-import getopt,sys,re,argparse
-from xml.dom.minidom import *
+import os,sys,re,argparse
+import xml.dom.minidom as minidom
 # usage for file in $(ls crab_0_121012_230338/res/crab_fjr_*.xml); do ../FileManagment/trunk/printGridUrlfromCrabReport.py --xmlFile $file --useAnalysisFile | grep srm; done >& Ntuple_files.txt
-def myGetSubNodeByName(node,name):
- if not node:
-   return None
- if not hasattr(node,'childNodes'):
-   return None
- for i,tmp_node in enumerate(node.childNodes):
-  if tmp_node.nodeName == name:
-   return tmp_node
- return None
+sys.path.append(os.getenv('CMSSW_BASE')+os.path.sep+'MyCMSSWAnalysisTools')
+from  CrabTools import  myGetSubNodeByName
 def getJobNode (args,jobNo):
   for cld in args.childNodes:
     if cld.nodeName =="Job":
       if cld.getAttribute("JobID") == jobNo:
         return cld
-
-parser = argparse.ArgumentParser()
-parser.add_argument('--xmlFile',default='',help=' input xmlFile')     
-parser.add_argument('--jobNum',nargs='+',default=[],help=' jobNumbers')
-args=parser.parse_args()
-dom = parse(args.xmlFile)
-if args.xmlFile == None or args.xmlFile =="" or (args.jobNum == []) :
- print "input wrong"
- sys.exit(1)
-arguments = myGetSubNodeByName(dom,"arguments")
-jobs = []
-for jobNo in args.jobNum:
-  job=getJobNode(arguments,jobNo)
-  if job.hasAttribute("MaxEvents"):
-    print job.getAttribute("MaxEvents")
-  jobs.append(job)
-# ExitCode
-#exitCode = myGetSubNodeByName(fwkRep,"ExitCode")
-#fwkExitCode =  myGetSubNodeByName(fwkRep,"FrameworkError")
-#fwkExitCode = fwkExitCode.getAttribute("ExitStatus")
-#if str(fwkExitCode) != "0":
-#  sys.exit("not all finished/done "+xmlFile+" "+str(fwkExitCode))
-#print "ExitCode ",exitCode.getAttribute("Value")
-## output files
-#if usePoolOutputFile:
-# poolOutputFile = myGetSubNodeByName(fwkRep,"File")
-# poolOutputFileGridUrl = myGetSubNodeByName(poolOutputFile,"SurlForGrid")
-# if poolOutputFileGridUrl:
-#   mystring=str(poolOutputFileGridUrl.firstChild.nodeValue).strip()
-#   if mystring:
-#     print mystring
-#   else:
-#     print "error ",xmlFile
-# else:
-#   print "error ",xmlFile
-#if useAnalysisFile:
-# analysisFile = myGetSubNodeByName(fwkRep,"AnalysisFile")
-# analysisFileGridUrl = myGetSubNodeByName(analysisFile,"SurlForGrid")
-# if analysisFileGridUrl:
-#   fileLoc = analysisFileGridUrl.getAttribute("Value") 
-#   test= re.sub('srm://grid-srm.physik.rwth-aachen.de:8443/srm/managerv2\?SFN=','dcap://grid-dcap.physik.rwth-aachen.de',fileLoc) #dcap:\/\/grid-dcap\.physik\.rwth-aachen\.de',fileLoc)
-#   if test:
-#     print  test
-#   else:
-#     print "error ",xmlFile
-# else:
-#  print "error ",xmlFile
+def getMaxEvents(xmlF,jobNos,debug = False):
+  maxEvents = {}
+  dom = minidom.parse(xmlF)
+  arguments = myGetSubNodeByName(dom,"arguments")
+  for jobNo in jobNos:
+    job=getJobNode(arguments,jobNo)
+    if job.hasAttribute("MaxEvents"):
+      if debug:
+        job.getAttribute("MaxEvents")
+      if not job.getAttribute("MaxEvents") =="-1":
+        maxEvents[job.getAttribute("JobID")] = job.getAttribute("MaxEvents")
+      else:
+        inputFiles=job.getAttribute('InputFiles').split(',')
+        import subprocess
+        dbsCommand='dbsql "find file.numevents where file='+' or file='.join(inputFiles)+'"'
+        if debug:
+          print dbsCommand
+        evtsFiles = [ int(no) for no in subprocess.Popen([dbsCommand],bufsize=1 , stdin=open(os.devnull),shell=True,stdout=subprocess.PIPE,env=os.environ).communicate()[0].split('\n') if no[:1].isdigit()]
+        if debug:
+          print evtsFiles, ' ' , job.getAttribute("JobID")
+        maxEvents[job.getAttribute("JobID")] = sum(evtsFiles) - int(job.getAttribute("SkipEvents"))
+      if debug:
+        print "maxEvents ",maxEvents[job.getAttribute("JobID")]
+  return maxEvents
+def main(): 
+  parser = argparse.ArgumentParser()
+  parser.add_argument('--xmlFile',default='',help=' input xmlFile')     
+  parser.add_argument('--jobNum',nargs='+',default=[],help=' jobNumbers')
+  args=parser.parse_args()
+  if args.xmlFile == None or args.xmlFile =="" or (args.jobNum == []) :
+    print "input wrong"
+    sys.exit(1)
+  maxEvents = getMaxEvents(args.xmlFile,args.jobNum) 
+  print " ".join(maxEvents.values())
+#####################
+if __name__ == "__main__":
+  main()
