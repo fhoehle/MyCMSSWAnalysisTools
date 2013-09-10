6756a169cdca743f1104020dc47a1dc71319c086
Felix Hoehle <hoehle@physik.rwth-aachen.de>
2013-09-10 15:58:25 +0200

    starting to use argparse

--- bin/myCrab.py
+++ bin/myCrab.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-import os,sys,re
+import os,sys,re,argparse
 sys.path.append(os.getenv('CMSSW_BASE')+'/MyCMSSWAnalysisTools/')
 import CrabTools
 files = [arg for arg in sys.argv[1:] if re.match('^[^-].*\.json$',arg) and os.path.isfile(arg)]
