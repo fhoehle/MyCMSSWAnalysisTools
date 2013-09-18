a97a0356b5abe9e8d7f3ab0a80f89b36edd3bcf1
Felix Hoehle <hoehle@physik.rwth-aachen.de>
2013-09-18 14:44:50 +0200

    using argparse

--- bin/myCrab.py
+++ bin/myCrab.py
@@ -2,17 +2,19 @@
 import os,sys,re,argparse
 sys.path.append(os.getenv('CMSSW_BASE')+'/MyCMSSWAnalysisTools/')
 import CrabTools
-files = [arg for arg in sys.argv[1:] if re.match('^[^-].*\.json$',arg) and os.path.isfile(arg)]
-for file in files:
-  sys.argv.remove(file)
-opts =  sys.argv[1:] 
-print "loading ",files
-if len(opts) > 2:
-  sys.exit("too many options, only one supported")
-for file in files:
+parser = argparse.ArgumentParser()
+parser.add_argument('jsonFiles',nargs='+',default=[],help=' input jsonFiles')
+parser.add_argument('--onlySummary',action='store_true',default=False,help='only summary')
+parser.add_argument('-automaticResubmit',action='store_true',default=False,help=' test Job and resubmit failed and get successful jobs')
+args , remaining= parser.parse_known_args()
+#files = [arg for arg in sys.argv[1:] if re.match('^[^-].*\.json$',arg) and os.path.isfile(arg)]
+for file in args.jsonFiles:
+  if not os.path.isfile(file):
+    sys.exit('not valid file '+file)
+for file in args.jsonFiles:
   testCrab =  CrabTools.loadCrabJob(file)
-  if "-automaticResubmit" in opts:
-    testCrab.automaticResubmit(False if not "-onlySummary" in opts else True)
+  if args.automaticResubmit:
+    testCrab.automaticResubmit(args.onlySummary)
   else:
-    testCrab.executeCrabCommand(" ".join(opts),debug=True)
+    testCrab.executeCrabCommand(" ".join(remaining),debug=True)
 
