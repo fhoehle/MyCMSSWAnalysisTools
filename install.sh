5e40575cfdc07a59f32fc51252b9760e1965aadf
Felix Hoehle <hoehle@physik.rwth-aachen.de>
2013-09-18 14:46:15 +0200

    added install script, needed for crabDeamon installation

---
+++ install.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+pkgs=(
+  "MyCrabTools ./ V00-01" 
+)
+cmsswVer=CMSSW_4_2_8_patch7
+###################
+function getGitPackage {
+
+if [ -d "$1" ]; then
+  echo "updating "$1
+  cd $1
+  git fetch
+else
+  echo "installing "$1
+  git clone git@github.com:fhoehle/$1.git
+  cd $1
+fi
+  
+}
+
+echo "Installing/Updating MyCMSSWAnalysisTools "
+#
+if [[ ! "$CMSSW_BASE" =~ "$cmsswVer" ]]; then
+  echo "missing CMSSW_BASE cmsenv"
+fi
+cd $CMSSW_BASE
+set -e
+# install my packages
+for idx in ${!pkgs[*]}; do
+  cd $CMSSW_BASE/`echo ${pkgs[$idx]} | awk '{print $2}'`
+  getGitPackage `echo ${pkgs[$idx]} | awk '{print $1}'`
+  git checkout `echo ${pkgs[$idx]} | awk '{print $3}'`
+  if  [ "X`echo ${pkgs[$idx]} | awk '{print $4}'`" != "X" ]; then
+    echo "calling additional command "`echo ${pkgs[$idx]} | awk '{print $4}'`
+    eval `echo ${pkgs[$idx]} | awk '{print $4}'`
+  fi
+  cd $CMSSW_BASE
+done
+
+
