7a6494cf7fdecfb541c1aef8cd8e9915a1e726b7
Felix Hoehle <hoehle@physik.rwth-aachen.de>
2013-11-27 11:19:52 +0100

    updated install

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 pkgs=(
-  "MyCrabTools ./ V00-01" 
+  "MyCrabTools ./ ./intall.sh" 
 )
 cmsswVer=CMSSW_4_2_8_patch7
 ###################
@@ -29,10 +29,10 @@ set -e
 for idx in ${!pkgs[*]}; do
   cd $CMSSW_BASE/`echo ${pkgs[$idx]} | awk '{print $2}'`
   getGitPackage `echo ${pkgs[$idx]} | awk '{print $1}'`
-  git checkout `echo ${pkgs[$idx]} | awk '{print $3}'`
-  if  [ "X`echo ${pkgs[$idx]} | awk '{print $4}'`" != "X" ]; then
-    echo "calling additional command "`echo ${pkgs[$idx]} | awk '{print $4}'`
-    eval `echo ${pkgs[$idx]} | awk '{print $4}'`
+  git checkout `echo ${pkgs[$idx]} | awk '{print $4}'`
+  if  [ "X`echo ${pkgs[$idx]} | awk '{print $3}'`" != "X" ]; then
+    echo "calling additional command "`echo ${pkgs[$idx]} | awk '{print $3}'`
+    eval `echo ${pkgs[$idx]} | awk '{print $3}'`
   fi
   cd $CMSSW_BASE
 done
