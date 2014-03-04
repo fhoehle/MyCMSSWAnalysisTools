f311de7d0eccedf981dc50c1e669a39a2d9646d4
Felix Hoehle <hoehle@physik.rwth-aachen.de>
2014-03-04 17:37:08 +0100

    add debug lumiTools

--- Tools/lumiTools.py
+++ Tools/lumiTools.py
@@ -2,9 +2,12 @@ import sys,os,re
 sys.path.append(os.getenv('CMSSW_BASE')+'/MyCMSSWAnalysisTools')
 import Tools.coreTools as coreTools
 from FWCore.PythonUtilities.LumiList   import LumiList
-def calcLumi(jsonFile):
+def calcLumi(jsonFile,addLumiOpt='',debug=False):
     csvFile=jsonFile.strip()+"_lumiCSV.csv"
-    lC = coreTools.executeCommandSameEnv("lumiCalc2.py overview -i "+jsonFile+" -o "+csvFile)
+    cmd="lumiCalc2.py "+addLumiOpt+" overview -i "+jsonFile+" -o "+csvFile
+    if debug:
+      print cmd
+    lC = coreTools.executeCommandSameEnv(cmd)
     coreTools.checkCommandAbortIfFail(lC)
     intLumi = 0
     for l in open(csvFile):
